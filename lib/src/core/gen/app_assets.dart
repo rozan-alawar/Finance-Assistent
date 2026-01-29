2b50bf568e57eaa49b5b630f1fb3f1d70297afa6
mustafa-shihab1 <mostafaa.sh2001@gmail.com>
2026-01-29 17:02:10 +0200

    add lib, assets and colors

--- lib/src/core/gen/app_assets.dart
+++ lib/src/core/gen/app_assets.dart
@@ -4,138 +4,157 @@
 class AppAssets {
   const AppAssets._();
 
-  /// ![preview](file:///Users/h/Desktop/Flutter projects.nosync/finance_assistent/assets/icons/apple.svg)
+  /// ![preview](file:///home/mustafa/StudioProjects/Finance-Assistent/assets/icons/apple.svg)
   static const String ASSETS_ICONS_APPLE_SVG = 'assets/icons/apple.svg';
 
-  /// ![preview](file:///Users/h/Desktop/Flutter projects.nosync/finance_assistent/assets/icons/arrow-left.svg)
+  /// ![preview](file:///home/mustafa/StudioProjects/Finance-Assistent/assets/icons/arrow-left.svg)
   static const String ASSETS_ICONS_ARROW_LEFT_SVG =
       'assets/icons/arrow-left.svg';
 
-  /// ![preview](file:///Users/h/Desktop/Flutter projects.nosync/finance_assistent/assets/icons/arrow.svg)
+  /// ![preview](file:///home/mustafa/StudioProjects/Finance-Assistent/assets/icons/arrow.svg)
   static const String ASSETS_ICONS_ARROW_SVG = 'assets/icons/arrow.svg';
 
-  /// ![preview](file:///Users/h/Desktop/Flutter projects.nosync/finance_assistent/assets/icons/debts.svg)
+  /// ![preview](file:///home/mustafa/StudioProjects/Finance-Assistent/assets/icons/debts.svg)
   static const String ASSETS_ICONS_DEBTS_SVG = 'assets/icons/debts.svg';
 
-  /// ![preview](file:///Users/h/Desktop/Flutter projects.nosync/finance_assistent/assets/icons/delete.svg)
+  /// ![preview](file:///home/mustafa/StudioProjects/Finance-Assistent/assets/icons/decrease-arrow.svg)
+  static const String ASSETS_ICONS_DECREASE_ARROW_SVG =
+      'assets/icons/decrease-arrow.svg';
+
+  /// ![preview](file:///home/mustafa/StudioProjects/Finance-Assistent/assets/icons/delete.svg)
   static const String ASSETS_ICONS_DELETE_SVG = 'assets/icons/delete.svg';
 
-  /// ![preview](file:///Users/h/Desktop/Flutter projects.nosync/finance_assistent/assets/icons/done.svg)
+  /// ![preview](file:///home/mustafa/StudioProjects/Finance-Assistent/assets/icons/done.svg)
   static const String ASSETS_ICONS_DONE_SVG = 'assets/icons/done.svg';
 
-  /// ![preview](file:///Users/h/Desktop/Flutter projects.nosync/finance_assistent/assets/icons/edit-doc.svg)
+  /// ![preview](file:///home/mustafa/StudioProjects/Finance-Assistent/assets/icons/edit-doc.svg)
   static const String ASSETS_ICONS_EDIT_DOC_SVG = 'assets/icons/edit-doc.svg';
 
-  /// ![preview](file:///Users/h/Desktop/Flutter projects.nosync/finance_assistent/assets/icons/edit.svg)
+  /// ![preview](file:///home/mustafa/StudioProjects/Finance-Assistent/assets/icons/edit.svg)
   static const String ASSETS_ICONS_EDIT_SVG = 'assets/icons/edit.svg';
 
-  /// ![preview](file:///Users/h/Desktop/Flutter projects.nosync/finance_assistent/assets/icons/email.svg)
+  /// ![preview](file:///home/mustafa/StudioProjects/Finance-Assistent/assets/icons/email.svg)
   static const String ASSETS_ICONS_EMAIL_SVG = 'assets/icons/email.svg';
 
-  /// ![preview](file:///Users/h/Desktop/Flutter projects.nosync/finance_assistent/assets/icons/error.svg)
+  /// ![preview](file:///home/mustafa/StudioProjects/Finance-Assistent/assets/icons/error.svg)
   static const String ASSETS_ICONS_ERROR_SVG = 'assets/icons/error.svg';
 
-  /// ![preview](file:///Users/h/Desktop/Flutter projects.nosync/finance_assistent/assets/icons/expenses-limit.svg)
+  /// ![preview](file:///home/mustafa/StudioProjects/Finance-Assistent/assets/icons/expenses-limit.svg)
   static const String ASSETS_ICONS_EXPENSES_LIMIT_SVG =
       'assets/icons/expenses-limit.svg';
 
-  /// ![preview](file:///Users/h/Desktop/Flutter projects.nosync/finance_assistent/assets/icons/expenses.svg)
+  /// ![preview](file:///home/mustafa/StudioProjects/Finance-Assistent/assets/icons/expenses.svg)
   static const String ASSETS_ICONS_EXPENSES_SVG = 'assets/icons/expenses.svg';
 
-  /// ![preview](file:///Users/h/Desktop/Flutter projects.nosync/finance_assistent/assets/icons/google.svg)
+  /// ![preview](file:///home/mustafa/StudioProjects/Finance-Assistent/assets/icons/google.svg)
   static const String ASSETS_ICONS_GOOGLE_SVG = 'assets/icons/google.svg';
 
-  /// ![preview](file:///Users/h/Desktop/Flutter projects.nosync/finance_assistent/assets/icons/invoice.svg)
+  /// ![preview](file:///home/mustafa/StudioProjects/Finance-Assistent/assets/icons/increase-arrow.svg)
+  static const String ASSETS_ICONS_INCREASE_ARROW_SVG =
+      'assets/icons/increase-arrow.svg';
+
+  /// ![preview](file:///home/mustafa/StudioProjects/Finance-Assistent/assets/icons/invoice.svg)
   static const String ASSETS_ICONS_INVOICE_SVG = 'assets/icons/invoice.svg';
 
-  /// ![preview](file:///Users/h/Desktop/Flutter projects.nosync/finance_assistent/assets/icons/lock.svg)
+  /// ![preview](file:///home/mustafa/StudioProjects/Finance-Assistent/assets/icons/lock.svg)
   static const String ASSETS_ICONS_LOCK_SVG = 'assets/icons/lock.svg';
 
-  /// ![preview](file:///Users/h/Desktop/Flutter projects.nosync/finance_assistent/assets/icons/nav/budget.svg)
+  /// ![preview](file:///home/mustafa/StudioProjects/Finance-Assistent/assets/icons/money-recive.svg)
+  static const String ASSETS_ICONS_MONEY_RECIVE_SVG =
+      'assets/icons/money-recive.svg';
+
+  /// ![preview](file:///home/mustafa/StudioProjects/Finance-Assistent/assets/icons/money-send.svg)
+  static const String ASSETS_ICONS_MONEY_SEND_SVG =
+      'assets/icons/money-send.svg';
+
+  /// ![preview](file:///home/mustafa/StudioProjects/Finance-Assistent/assets/icons/nav/budget.svg)
   static const String ASSETS_ICONS_NAV_BUDGET_SVG =
       'assets/icons/nav/budget.svg';
 
-  /// ![preview](file:///Users/h/Desktop/Flutter projects.nosync/finance_assistent/assets/icons/nav/budget_filled.svg)
+  /// ![preview](file:///home/mustafa/StudioProjects/Finance-Assistent/assets/icons/nav/budget_filled.svg)
   static const String ASSETS_ICONS_NAV_BUDGET_FILLED_SVG =
       'assets/icons/nav/budget_filled.svg';
 
-  /// ![preview](file:///Users/h/Desktop/Flutter projects.nosync/finance_assistent/assets/icons/nav/home.svg)
+  /// ![preview](file:///home/mustafa/StudioProjects/Finance-Assistent/assets/icons/nav/home.svg)
   static const String ASSETS_ICONS_NAV_HOME_SVG = 'assets/icons/nav/home.svg';
 
-  /// ![preview](file:///Users/h/Desktop/Flutter projects.nosync/finance_assistent/assets/icons/nav/home_filled.svg)
+  /// ![preview](file:///home/mustafa/StudioProjects/Finance-Assistent/assets/icons/nav/home_filled.svg)
   static const String ASSETS_ICONS_NAV_HOME_FILLED_SVG =
       'assets/icons/nav/home_filled.svg';
 
-  /// ![preview](file:///Users/h/Desktop/Flutter projects.nosync/finance_assistent/assets/icons/nav/reminder.svg)
+  /// ![preview](file:///home/mustafa/StudioProjects/Finance-Assistent/assets/icons/nav/reminder.svg)
   static const String ASSETS_ICONS_NAV_REMINDER_SVG =
       'assets/icons/nav/reminder.svg';
 
-  /// ![preview](file:///Users/h/Desktop/Flutter projects.nosync/finance_assistent/assets/icons/nav/reminder_filled.svg)
+  /// ![preview](file:///home/mustafa/StudioProjects/Finance-Assistent/assets/icons/nav/reminder_filled.svg)
   static const String ASSETS_ICONS_NAV_REMINDER_FILLED_SVG =
       'assets/icons/nav/reminder_filled.svg';
 
-  /// ![preview](file:///Users/h/Desktop/Flutter projects.nosync/finance_assistent/assets/icons/nav/service.svg)
+  /// ![preview](file:///home/mustafa/StudioProjects/Finance-Assistent/assets/icons/nav/service.svg)
   static const String ASSETS_ICONS_NAV_SERVICE_SVG =
       'assets/icons/nav/service.svg';
 
-  /// ![preview](file:///Users/h/Desktop/Flutter projects.nosync/finance_assistent/assets/icons/nav/service_filled.svg)
+  /// ![preview](file:///home/mustafa/StudioProjects/Finance-Assistent/assets/icons/nav/service_filled.svg)
   static const String ASSETS_ICONS_NAV_SERVICE_FILLED_SVG =
       'assets/icons/nav/service_filled.svg';
 
-  /// ![preview](file:///Users/h/Desktop/Flutter projects.nosync/finance_assistent/assets/icons/notification.svg)
+  /// ![preview](file:///home/mustafa/StudioProjects/Finance-Assistent/assets/icons/notification.svg)
   static const String ASSETS_ICONS_NOTIFICATION_SVG =
       'assets/icons/notification.svg';
 
-  /// ![preview](file:///Users/h/Desktop/Flutter projects.nosync/finance_assistent/assets/icons/phone.svg)
+  /// ![preview](file:///home/mustafa/StudioProjects/Finance-Assistent/assets/icons/phone.svg)
   static const String ASSETS_ICONS_PHONE_SVG = 'assets/icons/phone.svg';
 
-  /// ![preview](file:///Users/h/Desktop/Flutter projects.nosync/finance_assistent/assets/icons/revenues.svg)
+  /// ![preview](file:///home/mustafa/StudioProjects/Finance-Assistent/assets/icons/revenues.svg)
   static const String ASSETS_ICONS_REVENUES_SVG = 'assets/icons/revenues.svg';
 
-  /// ![preview](file:///Users/h/Desktop/Flutter projects.nosync/finance_assistent/assets/icons/search.svg)
+  /// ![preview](file:///home/mustafa/StudioProjects/Finance-Assistent/assets/icons/search.svg)
   static const String ASSETS_ICONS_SEARCH_SVG = 'assets/icons/search.svg';
 
-  /// ![preview](file:///Users/h/Desktop/Flutter projects.nosync/finance_assistent/assets/icons/suggestions.svg)
+  /// ![preview](file:///home/mustafa/StudioProjects/Finance-Assistent/assets/icons/suggestions.svg)
   static const String ASSETS_ICONS_SUGGESTIONS_SVG =
       'assets/icons/suggestions.svg';
 
-  /// ![preview](file:///Users/h/Desktop/Flutter projects.nosync/finance_assistent/assets/icons/us-flag.svg)
+  /// ![preview](file:///home/mustafa/StudioProjects/Finance-Assistent/assets/icons/us-flag.svg)
   static const String ASSETS_ICONS_US_FLAG_SVG = 'assets/icons/us-flag.svg';
 
-  /// ![preview](file:///Users/h/Desktop/Flutter projects.nosync/finance_assistent/assets/icons/user.svg)
+  /// ![preview](file:///home/mustafa/StudioProjects/Finance-Assistent/assets/icons/user.svg)
   static const String ASSETS_ICONS_USER_SVG = 'assets/icons/user.svg';
 
-  /// ![preview](file:///Users/h/Desktop/Flutter projects.nosync/finance_assistent/assets/icons/warning.svg)
+  /// ![preview](file:///home/mustafa/StudioProjects/Finance-Assistent/assets/icons/warning.svg)
   static const String ASSETS_ICONS_WARNING_SVG = 'assets/icons/warning.svg';
 
-  /// ![preview](file:///Users/h/Desktop/Flutter projects.nosync/finance_assistent/assets/images/apple.png)
+  /// ![preview](file:///home/mustafa/StudioProjects/Finance-Assistent/assets/images/apple.png)
   static const String ASSETS_IMAGES_APPLE_PNG = 'assets/images/apple.png';
 
-  /// ![preview](file:///Users/h/Desktop/Flutter projects.nosync/finance_assistent/assets/images/avatar.png)
+  /// ![preview](file:///home/mustafa/StudioProjects/Finance-Assistent/assets/images/avatar.png)
   static const String ASSETS_IMAGES_AVATAR_PNG = 'assets/images/avatar.png';
 
-  /// ![preview](file:///Users/h/Desktop/Flutter projects.nosync/finance_assistent/assets/images/google.png)
+  /// ![preview](file:///home/mustafa/StudioProjects/Finance-Assistent/assets/images/google.png)
   static const String ASSETS_IMAGES_GOOGLE_PNG = 'assets/images/google.png';
 
-  /// ![preview](file:///Users/h/Desktop/Flutter projects.nosync/finance_assistent/assets/images/logo.png)
+  /// ![preview](file:///home/mustafa/StudioProjects/Finance-Assistent/assets/images/logo.png)
   static const String ASSETS_IMAGES_LOGO_PNG = 'assets/images/logo.png';
 
-  /// ![preview](file:///Users/h/Desktop/Flutter projects.nosync/finance_assistent/assets/images/no-reports.png)
+  /// ![preview](file:///home/mustafa/StudioProjects/Finance-Assistent/assets/images/no-reports.png)
   static const String ASSETS_IMAGES_NO_REPORTS_PNG =
       'assets/images/no-reports.png';
 
-  /// ![preview](file:///Users/h/Desktop/Flutter projects.nosync/finance_assistent/assets/images/onboarding.png)
+  /// ![preview](file:///home/mustafa/StudioProjects/Finance-Assistent/assets/images/onboarding.png)
   static const String ASSETS_IMAGES_ONBOARDING_PNG =
       'assets/images/onboarding.png';
 
-  /// ![preview](file:///Users/h/Desktop/Flutter projects.nosync/finance_assistent/assets/images/placeholder.png)
+  /// ![preview](file:///home/mustafa/StudioProjects/Finance-Assistent/assets/images/placeholder.png)
   static const String ASSETS_IMAGES_PLACEHOLDER_PNG =
       'assets/images/placeholder.png';
 
-  /// ![preview](file:///Users/h/Desktop/Flutter projects.nosync/finance_assistent/assets/images/test.png)
+  /// ![preview](file:///home/mustafa/StudioProjects/Finance-Assistent/assets/images/splash.png)
+  static const String ASSETS_IMAGES_SPLASH_PNG = 'assets/images/splash.png';
+
+  /// ![preview](file:///home/mustafa/StudioProjects/Finance-Assistent/assets/images/test.png)
   static const String ASSETS_IMAGES_TEST_PNG = 'assets/images/test.png';
 
-  /// ![preview](file:///Users/h/Desktop/Flutter projects.nosync/finance_assistent/assets/images/user_placeholder.png)
+  /// ![preview](file:///home/mustafa/StudioProjects/Finance-Assistent/assets/images/user_placeholder.png)
   static const String ASSETS_IMAGES_USER_PLACEHOLDER_PNG =
       'assets/images/user_placeholder.png';
 }
