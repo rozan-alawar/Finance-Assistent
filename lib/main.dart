10134c763bb95343550e31afa9cd0f2bbdb20194
Marah Saadeh <marah4saadeh@gmail.com.com>
2026-02-06 03:59:06 +0200

    Add Empty Report page

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,3 @@
-
-
 import 'package:finance_assistent/src/core/config/theme/app_theme.dart';
 import 'package:finance_assistent/src/core/routing/app_route.dart';
 import 'package:finance_assistent/src/core/routing/navigation_service.dart';
@@ -13,13 +11,15 @@ import 'package:finance_assistent/src/core/services/sync/sync_service.dart';
 import 'package:finance_assistent/src/features/profile/presentation/pages/profile_page.dart';
 import 'package:finance_assistent/src/features/profile/presentation/pages/accounting_page.dart';
 import 'package:finance_assistent/src/features/Reports/presentation/pages/reports_page.dart';
-import 'package:finance_assistent/src/features/Reports/presentation/pages/no_report.dart';
+import 'package:finance_assistent/src/features/Reports/presentation/pages/empty_report.dart';
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await HiveService.init();
   SyncService().init();
   runApp(const MyApp());
 }
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
@@ -34,16 +34,13 @@ class MyApp extends StatelessWidget {
       create: (context) => AuthCubit()..checkSession(),
       child: Builder(
         builder: (context) {
-          // --- تم التعديل هنا لفتح شاشة التعديل مباشرة ---
-          return MaterialApp( // شلنا .router مؤقتاً
+          return MaterialApp.router(
             debugShowCheckedModeBanner: false,
             title: 'Finance Assistent',
-            // استخدمنا نفس الثيم الخاص بكم
-            theme: AppTheme(themeMode: AppThemeMode.light).getThemeData('ExpoArabic'),
-            
-            // هنا وضعنا الشاشة التي نريد اختبارها
-            home: const NoReportPage(), 
-            
+            theme: AppTheme(
+              themeMode: AppThemeMode.light,
+            ).getThemeData('ExpoArabic'),
+            routerConfig: goRouter(context.read<AuthCubit>()),
             builder: (_, child) {
               return GestureDetector(
                 onTap: NavigationService.removeFocus,
@@ -51,7 +48,7 @@ class MyApp extends StatelessWidget {
               );
             },
           );
-        }
+        },
       ),
     );
   }
